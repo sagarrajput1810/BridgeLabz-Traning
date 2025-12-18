342db9e2334ed0cd72871c0c2573360c04bf88af
Sagar Rajput <sagarrajput1810@gmail.com>
2025-12-18 16:58:21 +0530

    [Sagar Rajput] 3 more basic code

---
+++ core-csharp-practice/leetcode-codebase/Pow.cs
@@ -0,0 +1,8 @@
+using System;
+class Pow{
+	static void Main(){
+		int a = int.Parse(Console.ReadLine());
+		int b = int.Parse(Console.ReadLine());
+		Console.Write(Math.Pow(a,b));
+	}
+}
---
+++ core-csharp-practice/leetcode-codebase/Rectangle.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Rectangle{
+	static void Main(){
+		int l = int.Parse(Console.ReadLine());
+		int b = int.Parse(Console.ReadLine());
+		Console.Write(l*b);
+	}
+}
