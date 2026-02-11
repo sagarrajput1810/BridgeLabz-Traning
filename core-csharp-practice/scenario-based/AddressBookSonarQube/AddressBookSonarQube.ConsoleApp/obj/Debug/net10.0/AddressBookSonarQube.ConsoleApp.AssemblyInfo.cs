ccdaecabbb4acc19c7f465d6dd7c9df6b5121559
Sagar Rajput <sagarrajput1810@gmail.com>
2026-02-11 10:46:49 +0530

    [Sagar Rajput]

--- core-csharp-practice/scenario-based/AddressBookSonarQube/AddressBookSonarQube.ConsoleApp/obj/Debug/net10.0/AddressBookSonarQube.ConsoleApp.AssemblyInfo.cs
+++ core-csharp-practice/scenario-based/AddressBookSonarQube/AddressBookSonarQube.ConsoleApp/obj/Debug/net10.0/AddressBookSonarQube.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBookSonarQube.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2773c430a943a752564366ba5c3e347d5a76937")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a639369ccaf3dfe8777bb07df763e9326fb6e13c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBookSonarQube.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBookSonarQube.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
